2292f87d63f316d0407d130bd172a9657479f304
ūśēr̥ <r@DESKTOP-KUEKH95>
2024-03-27 20:47:40 +0530

    mysecondproject

---
+++ javaprogram/src/main/java/javaprogram/Addition.java
@@ -0,0 +1,14 @@
+package javaprogram;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=10;
+		int sum=a+b;
+		System.out.println(sum);
+
+	}
+
+}
